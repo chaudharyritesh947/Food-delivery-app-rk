d07ef61039437f773283152ce54df617d92fbddf
ritesh <ritesh@gopazo.com>
2021-12-17 03:53:58 +0530

    Commiting util

---
+++ util/sendMail.js
@@ -0,0 +1,33 @@
+var mailgun = require('mailgun-js')({apiKey: process.env.API_KEY, domain: process.env.DOMAIN});
+
+const sendEmail = (data) => {
+  const orderItems = JSON.parse(JSON.stringify(data.cartData));
+  let items = '';
+
+  //making text for all the items purchased
+   orderItems.items.forEach((element)=>{
+    items = items + `Name: ${element.name} Description: ${element.description} Price: ${element.price} \n`;
+   });
+
+   //making data object
+    var data = {
+        from: 'Food delivery app <chaudharyritesh947@gmail.com>',
+        to: data.user.email,
+        subject: 'Thanks for order with FDA',
+        text: `Total amount to be paid is: ${orderItems.amount} \n ${items} `,
+
+    };
+
+    //sending mail via mailgun
+    mailgun.messages().send(data, function (error, body) {
+        console.log(body);
+    });
+}
+
+module.exports = {
+    sendEmail
+}
+
+
+
+ 
